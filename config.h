8268c9e910917a7ebf81c83cc9af8a649731cdf1
Nikolay Antonov <ostinru@gmail.com>
2013-01-07 17:56:57 +0400

    Build project by autoconf:  * move sources to src directory  * remove old configure  * tweak Makefiles; move to the Makefile.in  * add configure.ac

--- config.h
+++
@@ -1,8 +0,0 @@
-#ifndef MRIM_CONFIG_H
-#define MRIM_CONFIG_H
-
-#define BUILD_NUMBER "git20120218"
-
-#define ENABLE_GTK
-
-#endif
---
+++ src/config.h
@@ -0,0 +1,5 @@
+#ifndef CONFIG_H
+	#define CONFIG_H
+	/* config from autotools */
+	#include "config_generated.h"
+#endif
